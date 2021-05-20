02173562442b63d1f94c6b4ed3f182c29dd029b2
Emmanuel <emmanu-0000@hotmail.com>
2021-05-20 13:05:43 -0500

    Stable versión of: Storage persist, tailwind, landing desktop

--- Components/reducers/index.js
+++ Components/reducers/index.js
@@ -1,19 +1,48 @@
-import { HANDLE_STORE_VOTES, HANDLE_STORE_CELEBRITIES } from "./types";
+import {
+  HANDLE_STORE_VOTES,
+  HANDLE_STORE_CELEBRITIES,
+  HANDLE_UPDATE_CELEBRITIES,
+} from "./types";
 
 import initialState from "../reducers/initialState";
 
 export const componentReducer = (state = initialState, action) => {
-  switch (action.type) {
+  let payload = action.payload;
+  let type = action.type;
+  switch (type) {
     case HANDLE_STORE_CELEBRITIES: {
       return {
         ...state,
         celebrities: action.payload,
       };
     }
+    case HANDLE_UPDATE_CELEBRITIES: {
+      return {
+        ...state,
+        celebrities: state.voting,
+      };
+    }
     case HANDLE_STORE_VOTES: {
       return {
         ...state,
-        counter: state.counter + action.value,
+        voting: state.celebrities.map((d) => {
+          if (d.name === payload.name) {
+            return {
+              ...d,
+              alreadyVote: payload.alreadyVote,
+              votingState: payload?.votingState,
+              voteType: payload?.voteType,
+              votes: {
+                positive: (d.votes.positive += payload?.positive),
+                negative: (d.votes.negative += payload?.negative),
+              },
+            };
+          } else {
+            return {
+              ...d,
+            };
+          }
+        }),
       };
     }
     default:
